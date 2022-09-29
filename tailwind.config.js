25350359b6ac9db5d5e7d483e7c77109052b190c
PJxDev <pedrocruces@hotmail.es>
2022-09-29 17:53:03 +0200

    colors configurated in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      
+      /* (main background) */
+      veryDarkBlue: "hsl(233, 47%, 7%)",
+      /* (card background) */
+      darkDesaturatedBlue: "hsl(244, 38%, 16%)",
+      /* (accent) */
+      softViolet: "hsl(277, 64%, 61%)",
+      /* (main heading, stats) */
+      white: "hsl(0, 0%, 100%)",
+      /* (main paragraph) */
+      slightlyTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
+      /* (stat headings) */
+      slightlyTransparentWhite2: "hsla(0, 0%, 100%, 0.6)",
+    
+    },
     extend: {},
   },
   plugins: [],
